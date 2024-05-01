fa5369de9f616400a40b5db05018e8d1a675cefe
Vini-Casa <dukeofgod@outlook.com>
2024-05-01 17:50:45 -0300

    tutorial react

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,52 @@
+import Colors from "@constants/Colors";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { Product } from "../types";
+import { Link } from "expo-router";
+
+export const defaultRestaurantImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={`menu/${product.id}`} asChild>
+      <Pressable style={styles.container} >
+        <Image
+          source={{ uri: product.image || defaultRestaurantImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "600",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
